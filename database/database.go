a7765eb6ef263c28fe0f1daf5c44bf8d62903d13
Emrul-Hasan-Emon <emrulhasanemon12@gmail.com>
2024-03-14 13:01:39 +0600

    Defined basic structure with some functionality.

---
+++ database/database.go
@@ -0,0 +1,8 @@
+package database
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+func DatabaseInstance() *mongo.Client {
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger()) // It is for showing the log in the console
 }
